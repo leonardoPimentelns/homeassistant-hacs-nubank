1c6f1951ca54d01d6a9d412653cad02e1a634af6
leonardopimentelns <leonardooliveirapimente@hotmail.com>
2022-06-30 15:39:18 -0300

    new version

--- custom_components/nubank/sensor.py
+++ custom_components/nubank/sensor.py
@@ -1,27 +1,20 @@
 """Platform for sensor integration."""
 from __future__ import annotations
 
-from datetime import timedelta,datetime
+from datetime import timedelta
 import logging
-from matplotlib.font_manager import json_dump
-from pynubank import Nubank, MockHttpClient
+from pynubank import Nubank
 import pandas as pd
 import voluptuous
-import json
+from homeassistant import const
+from homeassistant.helpers import entity
 from homeassistant import util
-from homeassistant.components.sensor import SensorEntity
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers import config_validation
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
-from homeassistant.helpers.typing import ConfigType, DiscoveryInfoType
 
 REQUIREMENTS = [
     "pynubank==2.17.0",
-    "requests==2.27.1",
-    "qrcode==7.3.1",
-    "pyOpenSSL==22.0.0",
-    "colorama==0.4.3",
-    "requests-pkcs12==1.13",
 ]
 
 _LOGGER = logging.getLogger(__name__)
@@ -30,12 +23,19 @@ CONF_CLIENT_ID = "client_id"
 CONF_CLIENT_SECRET = "client_secret"
 CONF_CLIENT_CERT = "client_cert"
 UPDATE_FREQUENCY = timedelta(minutes=10)
+FATURA = 'Fatura'
+CONTA = 'Conta'
+SENSOR_NAME = '{} {}'
 
 PLATFORM_SCHEMA = config_validation.PLATFORM_SCHEMA.extend(
     {
         voluptuous.Required(CONF_CLIENT_ID): config_validation.string,
         voluptuous.Required(CONF_CLIENT_SECRET): config_validation.string,
         voluptuous.Required(CONF_CLIENT_CERT): config_validation.string,
+        voluptuous.Optional(
+            const.CONF_NAME,
+            default=DEFAULT_NAME
+    ):      config_validation.string,
 
     }
 )
@@ -54,40 +54,72 @@ def setup_platform(
     nubank.authenticate_with_refresh_token(refresh_token, config[CONF_CLIENT_CERT])
     date = pd.to_datetime('today').date() + pd.DateOffset(day=7)
     due_date = date.strftime("%Y-%m-%d")
+    name = config.get(const.CONF_NAME)
 
-    add_entities([NuSensor(nubank,due_date)])
+    add_entities([FaturaSensor(nubank,due_date,name),ContaSensor(nubank,due_date,name)],True)
 
 
-class NuSensor(SensorEntity):
+class NuSensor(entity.Entity):
     """Representation of a pyNubank sensor."""
 
-    def __init__(self, nubank,due_date):
+    def __init__(self, nubank, due_date, name):
         """Initialize a new pyNubank sensor."""
-
+        self._name = name
+        self._state = const.STATE_UNKNOWN
         self._attr_name = "Nubank"
-        self.nubank = nubank
         self.due_date = due_date
+        self.nubank = nubank
         self.bills = None
+        self.status = None
         self.total_cumulative = None
         self.total_balance = None
         self.past_balance = None
         self.effective_due_date = None
         self.close_date = None
         self.total_bills = None
-
-       
-
+        self.transactions = None
+    @property
+    def extra_state_attributes(self):
+        """Return device specific state attributes."""
+        return self._attributes
 
     @util.Throttle(UPDATE_FREQUENCY)
     def update(self):
-        """Update state of sensor."""
-        self.account_balance = self.nubank.get_account_balance()
-        self._attr_native_value = self.account_balance
+        """Fetches new state data for the sensor."""
+        raise NotImplementedError
 
+    @property
+    def icon(self):
+        """Return icon."""
+        return "mdi:bank"
+
+    @property
+    def state(self):
+        """Returns the state of the sensor."""
+        return self._state
+
+    @property
+    def name(self):
+        """Returns the name of the sensor."""
+        return SENSOR_NAME.format(self._name, self._name_suffix)
+
+    @property
+    def _name_suffix(self):
+        """Returns the name suffix of the sensor."""
+        raise NotImplementedError
+
+
+class FaturaSensor(NuSensor):
+    """Representation of a pyNubank sensor."""
+
+    @property
+    def _name_suffix(self):
+        """Returns the name suffix of the sensor."""
+        return FATURA
+
+    @util.Throttle(UPDATE_FREQUENCY)
+    def update(self):
         self.bills= self.nubank.get_bills()
-       
-
-        self.transactions = self.nubank.get_card_statements()
         self.bills =[x for x  in self.bills if x['summary']['due_date'] > self.due_date]
         self.bills = pd.json_normalize(self.bills)
         count = len(self.bills)-1
@@ -98,37 +130,62 @@ class NuSensor(SensorEntity):
         self.total_cumulative = currency(self.total_cumulative)
         self.total_balance = currency(self.total_balance)
         self.past_balance = currency(self.past_balance)
-       
+
         self.effective_due_date = self.bills['summary.effective_due_date'][count]
         self.effective_due_date = format_date(self.effective_due_date)
 
         self.close_date = self.bills['summary.close_date'][count]
-        self.close_date = format_date(self.close_date) 
-        
+        self.close_date = format_date(self.close_date)
+
         self.total_bills = self.bills['summary.total_balance'].sum()
         self.total_bills = currency(self.total_bills)
-            
 
+        self.transactions= self.nubank.get_card_statements()
+        groupby_mounth = pd.DataFrame(self.transactions).get(['description', 'title', 'amount', 'time'])
+        self.transactions =[x for x  in self.transactions if x['time'] > self.due_date ]
+        self.transactions = pd.json_normalize(self.transactions)
+        self.transactions = pd.DataFrame(groupby_mounth).groupby(['title']).sum().to_json()
 
-    @property
-    def icon(self):
-        """Return icon."""
-        return "mdi:bank"
+        self._state = self.status
+        self._attributes = {}
+
 
     @property
     def extra_state_attributes(self):
-        """Return attribute sensor."""
-        attributes = {
+        """Return device specific state attributes."""
+        self._attributes = {
             "total_cumulative": self.total_cumulative,
             "total_balance": self.total_balance,
             "past_balance": self.past_balance,
             "effective_due_date" :  self.effective_due_date,
             "close_date" :  self.close_date,
-            "total_bills" :  self.total_bills
+            "total_bills" :  self.total_bills,
+            "transactions": self.transactions
         }
-        
-        return attributes
+        return  self._attributes
 
+class ContaSensor(NuSensor):
+    """Representation of a pyNubank sensor."""
+
+    @property
+    def _name_suffix(self):
+        """Returns the name suffix of the sensor."""
+        return CONTA
+
+    @util.Throttle(UPDATE_FREQUENCY)
+    def update(self):
+
+        self._state = self.status
+        self._attributes = {}
+
+    @property
+    def extra_state_attributes(self):
+        """Return device specific state attributes."""
+        self._attributes = {
+
+
+        }
+        return  self._attributes
 def currency(valor):
     valor =  "{:.2f}".format(valor/100)
     return valor
