2157e8182b387d8bdeb1deba1526297a8b6f5c4b
leonardoPimentelns <leonardooliveirapimentel@hotmail.com>
2022-07-25 16:14:32 -0300

    Update sensor.py

--- custom_components/nubank/sensor.py
+++ custom_components/nubank/sensor.py
@@ -3,7 +3,9 @@ from __future__ import annotations
 
 from datetime import timedelta
 import logging
+from matplotlib.font_manager import json_dump
 from pynubank import Nubank
+
 import pandas as pd
 import voluptuous
 import json
@@ -43,6 +45,7 @@ PLATFORM_SCHEMA = config_validation.PLATFORM_SCHEMA.extend(
             default=DEFAULT_NAME
     ):      config_validation.string,
 
+
     }
 )
 
@@ -55,10 +58,12 @@ def setup_platform(
 ):
     """Set up the pyNubank sensors."""
     nubank = Nubank()
+
     # nubank = Nubank(MockHttpClient())
     refresh_token = nubank.authenticate_with_cert(config[CONF_CLIENT_ID], config[CONF_CLIENT_SECRET], config[CONF_CLIENT_CERT])
     nubank.authenticate_with_refresh_token(refresh_token, config[CONF_CLIENT_CERT])
-    date = pd.to_datetime('today').date() + pd.offsets.MonthBegin(-1) + pd.DateOffset(day=7)
+    date = pd.to_datetime('today').date() + pd.offsets.MonthBegin() + pd.DateOffset(day=7)
+
     due_date = date.strftime("%Y-%m-%d")
     name = config.get(const.CONF_NAME)
 
@@ -79,12 +84,10 @@ class NuSensor(entity.Entity):
         self.status = None
         self.total_cumulative = None
         self.total_balance = None
-        self.past_balance = None
-        self.effective_due_date = None
-        self.close_date = None
         self.total_bills = None
         self.mouth_transactions = None
-        self.accont_balance: None
+        self.accont_balance= None
+        self.mouth_transactions_summary=None
 
     @property
     def extra_state_attributes(self):
@@ -128,62 +131,26 @@ class FaturaSensor(NuSensor):
     @util.Throttle(UPDATE_FREQUENCY)
     def update(self):
         self.bills= self.nubank.get_bills()
-        self.bills =[x for x  in self.bills if x['summary']['due_date'] > self.due_date]
-        self.bills = pd.json_normalize(self.bills)
-        count = len(self.bills)-1
+        self.bills =[x for x  in self.bills if x['summary']['due_date'] == self.due_date ]
+
+        for bills_details  in self.bills:
+            df_bills = self.nubank.get_bill_details(bills_details)
+            df_bills = pd.DataFrame(df_bills['bill']['line_items']).get(['amount','category'])
+            df_bills = pd.DataFrame(df_bills).groupby(['category']).sum()
+            df_bills = df_bills.loc[df_bills['amount'] > 0]
+            df_bills['amount'] = df_bills['amount']/100
+            df_bills['percent'] =  df_bills['amount'] /df_bills['amount'].sum() *100
+            df_bills['amount'] = df_bills['amount'].map('R${}'.format)
+            parsed = df_bills.to_json(orient="table",index=True,double_precision=2)
+            self.bills_mounth = json.loads(parsed)
+
+        for bills_details  in self.bills:
+            transactions = self.nubank.get_bill_details(bills_details)
+            self.mouth_transactions = transactions ['bill']['line_items']
 
 
 
-        self.status = self.bills['state'][count]
-
-        if self.status == "closed":
-            self.total_balance = self.bills['summary.remaining_balance'][count]
-        else:
-            self.total_balance = self.bills['summary.total_balance'][count]
-
-        self.total_balance = currency(self.total_balance)
-        self.total_cumulative = self.bills['summary.total_cumulative'][count]
-        self.past_balance = self.bills['summary.past_balance'][count]
-        self.total_cumulative = currency(self.total_cumulative)
-        self.past_balance = currency(self.past_balance)
-
-        transactions = self.nubank.get_card_statements()
-        start_date = pd.to_datetime('today').date() +pd.offsets.MonthBegin(-1) - pd.offsets.Day(+2)
-        end_date = pd.to_datetime('today').date() + pd.offsets.MonthBegin() - pd.offsets.Day(+2)
-
-        start_date = start_date.strftime("%Y-%m-%d")
-        end_date = end_date.strftime("%Y-%m-%d")
-
-
-
-        transactions =[x for x  in transactions if x['time'] > start_date < end_date ]
-        # df = pd.DataFrame(columns=['date','description','amount'])
-        # for item in transactions:
-        #     df.loc[len(df.index)] = [item['time'], item['description'],item['amount']/100]
-        # df['date'] = format_date_weekDay(df['date'])
-
-        groupby_mounth = pd.DataFrame(transactions).get(['description', 'title', 'amount', 'time'])
-
-        transactions = pd.DataFrame(groupby_mounth).groupby(['title']).sum()
-        transactions['amount'] = transactions['amount']/100
-        transactions['percent'] =  transactions['amount'] /transactions['amount'].sum() *100
-        transactions['amount'] = transactions['amount'].map('R${}'.format)
-        parsed = transactions.to_json(orient="table",index=True,double_precision=2)
-        self.mouth_transactions = json.loads(parsed)
-
-
-
-
-        self.effective_due_date = self.bills['summary.effective_due_date'][count]
-        self.effective_due_date = format_date(self.effective_due_date)
-
-        self.close_date = self.bills['summary.close_date'][count]
-        self.close_date = format_date(self.close_date)
-
-        self.total_bills = self.bills['summary.total_balance'].sum()
-        self.total_bills = currency(self.total_bills)
-
-        self._state = self.status
+        self._state = ''
         self._attributes = {}
 
 
@@ -192,13 +159,9 @@ class FaturaSensor(NuSensor):
     def extra_state_attributes(self):
         """Return device specific state attributes."""
         self._attributes = {
-            "total_cumulative": self.total_cumulative,
-            "past_balance": self.past_balance,
-            "effective_due_date" :  self.effective_due_date,
-            "close_date" :  self.close_date,
-            "total_bills" :  self.total_bills,
-            "total_balance": self.total_balance,
-            "mouth_transactions": self.mouth_transactions
+            "total_cumulative": self.bills_mounth,
+            "summary": self.bills,
+            "transactions": self.mouth_transactions
         }
         return  self._attributes
 
@@ -225,12 +188,12 @@ class ContaSensor(NuSensor):
 
         }
         return  self._attributes
-def currency(valor):
-    valor =  "{:.2f}".format(valor/100)
-    return valor
-def format_date(data):
-    data = pd.to_datetime(data).strftime("%d %b.")
-    return data
-def format_date_weekDay(data):
-    data = pd.to_datetime(data).dt.strftime("%a %d %b.")
-    return data
+# def currency(valor):
+#     valor =  "{:.2f}".format(valor/100)
+#     return valor
+# def format_date(data):
+#     data = pd.to_datetime(data).strftime("%d %b.")
+#     return data
+# def format_date_weekDay(data):
+#     data = pd.to_datetime(data).dt.strftime("%a %d %b.")
+#     return data
