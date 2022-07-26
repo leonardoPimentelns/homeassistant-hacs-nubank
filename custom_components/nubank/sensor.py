8e2d59591947eaac584c66b2cbc5d29b1c6fc23d
leonardoPimentelns <leonardooliveirapimentel@hotmail.com>
2022-07-26 10:23:04 -0300

    Update sensor.py

--- custom_components/nubank/sensor.py
+++ custom_components/nubank/sensor.py
@@ -148,8 +148,8 @@ class FaturaSensor(NuSensor):
             gb = self.nubank.get_bill_details(bills_details)
             transactions = pd.DataFrame(gb['bill']['line_items'])
             transactions['amount'] = transactions['amount']/100
-            transactions['post_date'] = format_date(transactions['post_date'])
             transactions['amount'] = transactions['amount'].map('R${}'.format)
+            transactions['post_date'] = format_date(transactions['post_date'])
             parsed = transactions.to_json(orient="table",index=True,double_precision=2)
             self.mouth_transactions = json.loads(parsed)
       
